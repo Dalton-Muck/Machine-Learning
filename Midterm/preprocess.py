2865f0981768e3f2e431c15141188ab9579aa743
Colinster327 <cmurph0327@gmail.com>
2025-03-02 22:04:17 -0500

    load mutations again (hopefully it works :/)

--- Midterm/preprocess.py
+++ Midterm/preprocess.py
@@ -11,35 +11,35 @@ from sklearn.utils import resample
 print('loading genes...')
 genes = pd.read_csv('../dataset.csv')
 
-# print('loading mutations...')
-# mutations_targets = np.genfromtxt(
-#     '../mutations.csv',
-#     delimiter=',',
-#     names=True,
-#     dtype=None,
-#     usecols=[0]
-# )
-# max_features = np.genfromtxt(
-#     '../mutations.csv',
-#     delimiter=',',
-#     max_rows=1,
-# ).shape[0]
-# mutations_features = np.genfromtxt(
-#     '../mutations.csv',
-#     delimiter=',',
-#     dtype=None,
-#     names=True,
-#     usecols=range(1, max_features)
-# )
-# mutations = pd.DataFrame(
-#     mutations_features,
-#     columns=mutations_features.dtype.names
-# )
-# mutations_targets = pd.DataFrame(
-#     mutations_targets,
-#     columns=mutations_targets.dtype.names
-# )
-# mutations = pd.concat([mutations_targets, mutations], axis=1)
+print('loading mutations...')
+mutations_targets = np.genfromtxt(
+    '../mutations.csv',
+    delimiter=',',
+    names=True,
+    dtype=None,
+    usecols=[0]
+)
+max_features = np.genfromtxt(
+    '../mutations.csv',
+    delimiter=',',
+    max_rows=1,
+).shape[0]
+mutations_features = np.genfromtxt(
+    '../mutations.csv',
+    delimiter=',',
+    dtype=None,
+    names=True,
+    usecols=range(1, max_features)
+)
+mutations = pd.DataFrame(
+    mutations_features,
+    columns=mutations_features.dtype.names
+)
+mutations_targets = pd.DataFrame(
+    mutations_targets,
+    columns=mutations_targets.dtype.names
+)
+mutations = pd.concat([mutations_targets, mutations], axis=1)
 
 # encode targets
 print('encoding gene targets...')
@@ -48,10 +48,10 @@ genes['targets'] = genes['targets'].apply(lambda x: re.sub(r'\d+', '', x))
 encoder = LabelEncoder()
 genes['targets'] = encoder.fit_transform(genes['targets'])
 
-# print('encoding mutation targets...')
-# mutations['targets'] = mutations['targets'].apply(
-#     lambda x: re.sub(r'\d+', '', x))
-# mutations['targets'] = LabelEncoder().fit_transform(mutations['targets'])
+print('encoding mutation targets...')
+mutations['targets'] = mutations['targets'].apply(
+    lambda x: re.sub(r'\d+', '', x))
+mutations['targets'] = LabelEncoder().fit_transform(mutations['targets'])
 
 # ensure equal samples for each class
 print('resampling genes...')
@@ -62,12 +62,12 @@ genes = pd.concat([
     for cls in genes['targets'].unique()
 ])
 
-# print('resampling mutations...')
-# mutations = pd.concat([
-#     resample(mutations[mutations['targets'] == cls], replace=False,
-#              n_samples=class_counts, random_state=420)
-#     for cls in mutations['targets'].unique()
-# ])
+print('resampling mutations...')
+mutations = pd.concat([
+    resample(mutations[mutations['targets'] == cls], replace=False,
+             n_samples=class_counts, random_state=420)
+    for cls in mutations['targets'].unique()
+])
 
 features = genes.drop('targets', axis=1)
 targets = genes['targets']
@@ -78,32 +78,19 @@ classif_functions = [
 for func in classif_functions:
     print(f'Applying {func.__name__} k=300')
     selector = SelectKBest(score_func=func, k=300)
-    selected_features = selector.fit_transform(features, targets)
+    selector.fit_transform(features, targets)
     filtered_genes = features.columns[selector.get_support()].tolist()
-    new_genes = pd.DataFrame(selected_features, columns=filtered_genes)
-    new_genes.index = targets
+    print('filtering mutations based off genes...')
+    print('number of mutations before: ', mutations.shape[1] - 1)
+    # Filter mutations to keep only the columns that match the selected gene features
+    pattern = '|'.join(filtered_genes)
+    mutations_filtered = mutations.loc[:, (mutations.columns.str.contains(
+        pattern) & mutations.nunique() > 1)]
+    print('number of mutations after: ', mutations_filtered.shape[1])
 
-    new_genes.to_csv(f'../genes_{func.__name__}_300.csv', index=True)
-
-
-# Apply SelectKBest
-selector = SelectKBest(score_func=f_classif, k=300)
-selected_features = selector.fit_transform(features, targets)
-
-# Get the mask of selected features
-# mask = selector.get_support()
-#
-# # Get the selected feature names
-# genes_features = features.columns[mask].tolist()
-#
-# print('filtering mutations based off genes...')
-# print('number of mutations before: ', mutations.shape[1] - 1)
-# # Filter mutations to keep only the columns that match the selected gene features
-# pattern = '|'.join(genes_features)
-# mutations_filtered = mutations.loc[:, (mutations.columns.str.contains(
-#     pattern) & mutations.nunique() > 1)]
-# print('number of mutations after: ', mutations_filtered.shape[1])
-#
-# print('saving mutations to csv...')
-# mutations_filtered.index = mutations['targets']
-# mutations_filtered.to_csv('../filtered_mutations.csv', index=True)
+    print('saving mutations to csv...')
+    mutations_filtered.index = mutations['targets']
+    mutations_filtered.to_csv(
+        f'../mutations_{func.__name__}_300.csv',
+        index=True
+    )
